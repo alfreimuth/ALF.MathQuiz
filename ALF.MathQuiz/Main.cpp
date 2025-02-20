63f0f93023fbe67a17fcbeb43f18ae2396fbb207
Alexander Freimuth <500211023@fvtc.edu>
2025-02-20 10:40:29 -0600

    Removed creation of TestQuestions. It should be done on users side.

--- ALF.MathQuiz/Main.cpp
+++ ALF.MathQuiz/Main.cpp
@@ -15,10 +15,6 @@ int main()
 {
 	string questions = "C:\\Temp\\TestQuestion.txt";
 	string answer = "c:\\Temp\\TestAnswers.txt";
-	
-	ofstream ofs(questions);
-	ofs << "1 + 1 = \n2 * 3 = \n4 / 2 = \n5 - 3 = \n6 + 4 = \n8 * 2 = \n10 / 5 = \n7 - 2 = \n9 + 1 = \n3 * 5 = "; // makes the question list. Is there a better way to do this?
-	ofs.close(); // close stream
 
 	string line;
 	ifstream ifs(questions);
@@ -27,11 +23,14 @@ int main()
 
 	ofstream ofs2(answer);
 
+	cout << "Assignment 5 - Math Quiz\n" << "Have the test questions prepared at C:\\Temp\\TestQuestion.txt\n";
+
+	srand(time(NULL)); // initialize rng
 
 	for (int i = 0; i < 3; i++)
 	{
 
-		srand(time(NULL)); // initialize rng
+		
 		int row = rand() % 10 + 1;
 
 		for (int i = 1; i <= row; i++)
@@ -44,6 +43,8 @@ int main()
 		getline(cin, input);
 		ofs2 << line << input << "\n";
 	}
+
+	cout << "Results saved at c:\\Temp\\TestAnswers.txt";
 
 	ofs2.close();
 
